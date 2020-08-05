7ac0d7587b2d687e6ae3b0febf66d7c5a5f37997
AmorFatiBA <you@example.com>
2020-08-05 17:40:35 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace deneme19_NameHiding
+{
+    class Temel
+    {
+        public int a;
+        public void Goster()
+        {
+            Console.WriteLine("Temel.a =" + a);
+        }
+    }
+
+    class Tureyen : Temel
+    {
+        new int a;
+
+        public Tureyen(int aTemel, int aTureyen)
+        {
+            a = aTureyen;
+            base.a = aTemel;
+        }
+        new public void Goster()
+        {
+            base.Goster();
+            Console.WriteLine("Türeyen.a = " + a);
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!\n");
+
+            Tureyen nesne = new Tureyen(5, 6);
+            nesne.Goster();
+
+        }
+    }
+}
